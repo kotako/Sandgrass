8fd268da9c88cdbb81d4e0a727e62d7c43f369fa
Takorras <sy.empress0321@gmail.com>
2017-12-20 15:52:57 +0900

    [add]add redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import 'semantic-ui-css/semantic.min.css';
 import App from './components/App';
 import sandglass from './reducers'
 import registerServiceWorker from './config/registerServiceWorker';
 
-let store = createStore(sandglass)
+let store = createStore(sandglass, applyMiddleware(thunk));
 
 render(
   <Provider store={store}>

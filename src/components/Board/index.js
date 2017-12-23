6bfe8696e9f0e6f3db643b8bc3747668ff89d992
Takorras <sy.empress0321@gmail.com>
2017-12-24 01:29:34 +0900

    [add]add flips counter

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import TweetsContainer from '../../containers/TweetsContainer';
 import ContributionContainer from '../../containers/ContributionContainer';
 import TimerContainer from '../../containers/TimerContainer';
+import FlipsContainer from '../../containers/FlipsContainer';
 import QiitaCardContainer from '../../containers/QiitaCardContainer';
 
 const Wrapper = styled.div`
@@ -14,6 +15,7 @@ const Board = () => (
     <TimerContainer />
     <ContributionContainer />
     <TweetsContainer/>
+    <FlipsContainer/>
     <QiitaCardContainer />
   </Wrapper>
 )

21fc42d814570831e6d57ef12cdb032fbb7b6309
Takorras <sy.empress0321@gmail.com>
2017-12-29 00:20:50 +0900

    [delete]delete auth reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,14 +3,12 @@ import timer from './timer';
 import user from './user';
 import contributions from './contributions';
 import qiitaCard from './qiitaCard';
-import auth from './auth';
 
 const sandglass = combineReducers({
   timer,
   user,
   contributions,
   qiitaCard,
-  auth
 })
 
 export default sandglass;

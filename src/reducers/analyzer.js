6ac2c67a9a65499b3d14389cbc0fadf40e571beb
Takorras <sy.empress0321@gmail.com>
2017-12-31 20:50:00 +0900

    [add]add analyzer/chart reducer

--- src/reducers/analyzer.js
+++ src/reducers/analyzer.js
@@ -3,7 +3,34 @@ import Moment from 'moment';
 const analyzer = (state ={}, action) => {
   switch (action.type) {
     case 'INIT':
-      return {...state, leftDataKey: 'working_time', rightDataKey: 'commits', threshold: 86400, border: Moment().day(-6).startOf('date').unix(), format: 'MM/DD' };
+      return {
+        ...state,
+        leftDataKey: 'working_time',
+        rightDataKey: 'commits',
+        threshold: 86400,
+        border: Moment().day(-6).startOf('date').unix(),
+        format: 'MM/DD'
+      };
+    case 'RIGHT_CHART_CHANGED':
+      return {
+        ...state,
+        rightDataKey: action.rightDataKey
+      };
+    case 'LEFT_CHART_CHANGED':
+      return {
+        ...state,
+        leftDataKey: action.leftDataKey
+      };
+    case 'BORDER_CHANGED':
+      return {
+        ...state,
+        border: action.border
+      }
+    case 'THRESHOLD_CHANGED':
+      return {
+        ...state,
+        threshold: action.threshold
+      }
     default:
       return state;
   }
